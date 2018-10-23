b1fa5afe5b16fb90e5d0ef6053e10ba705f0603f
Kevin giordani <giordani.kevin.02@gmail.com>
2018-10-23 17:13:58 +0200

    convert timestamp to morning, afternoon, night

--- src/main/scala/main/DataCleaner.scala
+++ src/main/scala/main/DataCleaner.scala
@@ -1,7 +1,36 @@
 package main
 import org.apache.spark.sql.DataFrame
+import java.text.SimpleDateFormat
+import java.util.{Calendar, Date}
+import org.apache.spark.sql.functions.udf
 
 object DataCleaner {
+
+    def discretizeTimestamp(data: DataFrame): DataFrame = {
+        def discretize(hourOfTheDay: Int) : String = {
+            hourOfTheDay match {
+                case hourOfTheDay if hourOfTheDay >= 20 || hourOfTheDay < 4 => "Night"
+                case hourOfTheDay if hourOfTheDay >= 4 && hourOfTheDay < 12  => "Morning"
+                case hourOfTheDay if hourOfTheDay >= 12 && hourOfTheDay < 20  => "Afternoon"
+            }
+        }
+        def getCurrentHour(timestamp: Long): Int = {
+            try {
+                Integer.parseInt(new SimpleDateFormat("HH")format(new Date(timestamp*1000)))
+            } catch {
+                case _ => return 0
+            } 
+        }
+        def mapTimestamp(timestamp: String) : Option[String]  = {
+            if(timestamp == null) null
+            else{
+                Some(discretize(getCurrentHour(timestamp.toLong)))  
+            }
+        }
+        val discret = udf[Option[String], String](mapTimestamp)
+        val dataCleaned = data.withColumn("timestamp", discret(data("timestamp")))
+        dataCleaned
+    }
 
     def cleanData(data: DataFrame): DataFrame = {
         data
